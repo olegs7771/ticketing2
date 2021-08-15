f12b2bc18a0e8fc41029b305c0424849d203a949
olegs7771 <olegs7771@gmail.com>
2021-08-15 15:29:00 -0500

    secret key as procces.env.JWT_KEY

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -31,8 +31,8 @@ router.post(
     //Generate jwt
     const userJwt = jwt.sign(
       { id: newUser.id, email: newUser.email },
-      // process.env.JWT_KEY!,
-      'asdf',
+      process.env.JWT_KEY!,
+      // 'asdf',
     );
     //Store it on session object
     req.session = {
